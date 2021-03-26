5f8b5cde7a636f4e527d61e47fe0d22912c0322b
unknown <Your Name>
2021-03-26 14:53:58 +0900

    초기 퍼블리싱 업로드

---
+++ www/inc/header.php
@@ -0,0 +1,169 @@
+        <div id="quick">
+			<a href="#" class="icon1">
+				<i></i><span>공지사항</span>
+			</a>
+			<a href="#" class="icon2">
+				<i></i><span>진료예약</span>
+			</a>
+			<a href="#" class="icon3">
+				<i></i><span>진료시간</span>
+			</a>
+			<a href="#" class="icon4">
+				<i></i><span>오시는길</span>
+			</a>
+			<a href="#" class="blog" target="_blank">
+				<i></i><span>네이버블로그</span>
+			</a>
+			<a href="#" class="kakao" target="_blank">
+				<i></i><span>카톡상담</span>
+			</a>
+		</div>
+		<header>
+			<div class="nav-top">
+				<div class="inner">
+					<div class="nav-sns">
+						<a href="#" class="blog" title="블로그 바로가기">
+							<img src="/resource/images/w-blog.png" alt="">
+						</a>
+						<a href="#" class="kakao" title="카카오 바로가기">
+							<img src="/resource/images/w-kakao.png" alt="">
+						</a>
+					</div>
+					<div class="nav-login">
+						<a href="#" class="login">간편로그인</a>
+						<!-- 
+											<span class="username">mediinside</span>
+											<a href="#" class="logout">로그아웃</a>
+										 -->
+					</div>
+				</div>
+			</div>
+			<div class="nav-middle inner">
+				<div class="bnnr">
+					<img src="/resource/images/side-logo.png" alt="대학교수 출신 안과전문의 2인진료">
+				</div>
+				<h1 class="logo">
+					<a href="/">
+						<img src="/resource/images/logo.png" alt="서울탑안과">
+					</a>
+				</h1>
+				<div class="center-tel">
+					<p>고객센터</p>
+					<a href="tel:0439038000">043.903.<span class="point-b">8000</span></a>
+				</div>
+			</div>
+			<div class="nav-bottom">
+				<ul class="menu inner">
+					<li>
+						<a href="#">병원소개</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="/intro/page01.html">병원소개</a>
+							</li>
+                            <li>
+                                <a href="/intro/page02.html">의료진소개</a>
+                            </li>
+                            <li>
+                                <a href="/intro/page03.html">장비소개</a>
+                            </li>
+                            <li>
+                                <a href="/intro/page04.html">병원 둘러보기</a>
+                            </li>
+                            <li>
+                                <a href="/intro/page05.html">진료안내</a>
+                            </li>
+                            <li>
+                                <a href="/intro/page06.html">오시는길</a>
+                            </li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">백내장/노안</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="/eyemenu01/page01.html">노안/백내장이란</a>
+							</li>
+                            <li>
+								<a href="/eyemenu01/page02.html">백내장수술</a>
+							</li>
+                            <li>
+								<a href="/eyemenu01/page03.html">노안교정백내장수술</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">망막질환</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="/eyemenu02/page01.html">황반변성</a>
+							</li>
+                            <li>
+								<a href="/eyemenu02/page02.html">당뇨망막증</a>
+							</li>
+                            <li>
+								<a href="/eyemenu02/page03.html">망막앞막</a>
+							</li>
+                            <li>
+								<a href="/eyemenu02/page04.html">황반원공</a>
+							</li>
+                            <li>
+								<a href="/eyemenu02/page05.html">망막박리</a>
+							</li>
+                            <li>
+								<a href="/eyemenu02/page06.html">포도막염</a>
+							</li>
+                            <li>
+								<a href="/eyemenu02/page07.html">망막정맥폐쇄</a>
+							</li>
+                            <li>
+								<a href="/eyemenu02/page08.html">중심장액맥락망막병증</a>
+							</li>
+                            <li>
+								<a href="/eyemenu02/page09.html">비문증</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">녹내장</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="/eyemenu03/page01.html">녹내장이란</a>
+							</li>
+                            <li>
+								<a href="/eyemenu03/page02.html">녹내장치료</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">안구건조증</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="/eyemenu04/page01.html">안구건조증</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">안구종합검진</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="/eyemenu05/page01.html">연령별 안구 종합검진</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">커뮤니티</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="">공지사항</a>
+							</li>
+                            <li>
+								<a href="">온라인상담</a>
+							</li>
+                            <li>
+								<a href="">수술후기</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</header>
---
+++ www/notice/location.php
@@ -0,0 +1,14 @@
+                <img src="/resource/images/sub-bnnr01.png" alt="">
+                <div class="cont">
+                    <span>TOP RETINA CENTER</span>
+                    <h2>커뮤니티</h2>
+                    <!-- <p></p> -->
+                    <i></i>
+                    <div id="location">
+                        <ul>
+                            <li><a href="/eyemenu01/page01.html">공지사항</a></li>
+                            <li><a href="/eyemenu01/page02.html">온라인상담</a></li>
+                            <li><a href="/eyemenu01/page03.html">수술후기</a></li>
+                        </ul>
+                    </div>
+                </div>
