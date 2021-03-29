72c8851b219c52a84a272afd6106b480f34bd76c
unknown <tkatls1013@naver.com>
2021-03-29 16:27:13 +0900

    병원소개 업로드

---
+++ www/intro/location.php
@@ -0,0 +1,20 @@
+                <img src="/resource/images/sub-bnnr01.png" alt="">
+                <div class="cont">
+                    <span>TOP RETINA CENTER</span>
+                    <h2>병원소개</h2>
+                    <p>
+                        청주에서 만나는 고품격 의료 서비스<br>
+                        대학병원급 최신 검사/수술 장비로 최선의 치료를 제공합니다
+                    </p>
+                    <i></i>
+                    <div id="location">
+                        <ul class="n4">
+                            <li><a href="/intro/page01.html">병원소개</a></li>
+                            <li><a href="/intro/page02.html">의료진 소개</a></li>
+                            <li><a href="/intro/page03.html">장비 소개</a></li>
+                            <li><a href="/intro/page04.html">병원 둘러보기</a></li>
+                            <li><a href="/intro/page05.html">진료안내</a></li>
+                            <li><a href="/intro/page06.html">오시는 길</a></li>
+                        </ul>
+                    </div>
+                </div>
