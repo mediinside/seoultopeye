798cd8f6412a7c02e37ea83f80c27402ca294aba
unknown <tkatls1013@naver.com>
2021-03-29 18:45:02 +0900

    녹내장 업로드

---
+++ www/eyemenu03/location.php
@@ -0,0 +1,16 @@
+                <img src="/resource/images/sub-bnnr03.png" alt="">
+                <div class="cont">
+                    <span>TOP RETINA CENTER</span>
+                    <h2>녹내장</h2>
+                    <p>
+                        녹내장은 병의 말기까지 증상이 없기 때문에<br>
+                        안과 정기 검진을 통한 조기발견과 치료가 무엇보다 중요합니다.
+                    </p>
+                    <i></i>
+                    <div id="location">
+                        <ul>
+                            <li><a href="/eyemenu01/page01.html">녹내장이란?</a></li>
+                            <li><a href="/eyemenu01/page02.html">녹내장치료</a></li>
+                        </ul>
+                    </div>
+                </div>
